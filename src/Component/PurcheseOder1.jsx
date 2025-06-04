9a2a8eaf6436f67c9c1972fe05c7a58122827803
prachinayak03 <nprachi860@gmail.com>
2025-06-04 18:56:10 +0530

    Update PurcheseOder1.jsx

--- src/Component/PurcheseOder1.jsx
+++ src/Component/PurcheseOder1.jsx
@@ -109,30 +109,11 @@ const formatDate = (dateString) => {
           
               <div className="col-md-6 box" style={{ position: "relative", left: "15%" }} id="col">
                   <h2 className={`${isGeneratingPDF ? 'd-none' : ''}`}>PO Detail</h2>
-
-                  {isGeneratingPDF ? (
-                  // Show bold PO # text when generating PDF
-              <div className="form-control mb-2" style={{ fontWeight: 'bold', fontSize: '20px' , border:'none' }}>
-                PO#
-                 <input className={`form-control mb-2 ${isGeneratingPDF ? 'border-0' : ''}`}
-            style={{
-              fontSize: isGeneratingPDF ? '20px' : '',
-              paddingLeft: isGeneratingPDF ? '0' : '',
-            }}
-            placeholder="PO #"
-          />
-              </div>
-      
-            ) : (
-            <input
-            className={`form-control mb-2 ${isGeneratingPDF ? 'border-0' : ''}`}
-            style={{
-              fontSize: isGeneratingPDF ? '20px' : '',
-              paddingLeft: isGeneratingPDF ? '0' : '',
-            }}
-            placeholder="PO #"
-          />
-          )}
+             <input className={`form-control mb-2 ${isGeneratingPDF ? 'border-0' : ''}`}
+              style ={{
+                    fontSize: isGeneratingPDF ? '20px' : '',
+                    paddingLeft: isGeneratingPDF ? '0' : '',
+                  }} placeholder="PO #"/>
 
                {isGeneratingPDF ? (
                <div className='mb-2'>
