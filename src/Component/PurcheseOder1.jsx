878e589164c981fd7543b89a9bf16ec737c8a339
prachinayak03 <nprachi860@gmail.com>
2025-06-04 18:23:13 +0530

    Update PurcheseOder1.jsx

--- src/Component/PurcheseOder1.jsx
+++ src/Component/PurcheseOder1.jsx
@@ -109,20 +109,18 @@ const formatDate = (dateString) => {
           
               <div className="col-md-6 box" style={{ position: "relative", left: "15%" }} id="col">
                   <h2 className={`${isGeneratingPDF ? 'd-none' : ''}`}>PO Detail</h2>
+                
               <input className={`form-control mb-2 ${isGeneratingPDF ? 'border-0' : ''}`}
               style={{
                     fontSize: isGeneratingPDF ? '20px' : '',
                     paddingLeft: isGeneratingPDF ? '0' : '',
-                  }}placeholder="PO #"/>
+                  }}>PO #</input>
 
                {isGeneratingPDF ? (
                <div className='mb-2'>
                  <p><strong style={isGeneratingPDF ? {fontSize:'20px'}:{}} >Order Date:</strong></p>
-                 <p style={{
-                    fontSize: isGeneratingPDF ? '20px' : '',
-                    display: isGeneratingPDF ? 'inline' : '',
-                  }} >{formatDate(orderDate)}</p>
-                 <p ><strong style={isGeneratingPDF ? {fontSize:'20px'}:{}}>Delivery Date:</strong></p>
+                 <p style={isGeneratingPDF ? {fontSize:'20px'}:{}}>{formatDate(orderDate)}</p>
+                 <p><strong style={isGeneratingPDF ? {fontSize:'20px'}:{}}>Delivery Date:</strong></p>
                  <p style={isGeneratingPDF ? {fontSize:'20px'}:{}}>{formatDate(deliveryDate)}</p>
                </div>
              ) : (
